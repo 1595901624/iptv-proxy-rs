dbf24ec4a9e4fa8ab545a4cbd996622981ef2b6b
luhaoyu <luhaoyu@cmhi.chinamobile.com>
2023-04-03 20:55:59 +0800

    proxy m3u8

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,35 @@ use std::convert::Infallible;
 use std::time::Duration;
 use reqwest::header::HeaderMap;
 use warp::Filter;
-use warp::http::{Response, Uri};
+use warp::http::{Response};
+use urlencoding::decode;
 
 #[tokio::main]
 async fn main() {
     let cors = warp::cors().allow_any_origin();
-    let url = "https://live.v1.mk/api/bestv.php?id=cctv1hd8m/8000000";
+    // let url = "https://live.v1.mk/api/bestv.php?id=cctv1hd8m/8000000";
+    // let url = "https://live.v1.mk/api/sxg.php?id=CCTV-6H265_4000";
 
-    let hello = warp::path!("hello_1.m3u8").and_then(move || {
-        get_content_async()
-    }).with(cors);
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    // proxy m3u8
+    let m3u8_proxy_router = warp::path!("m3u8" / String).and_then(move |url: String| {
+        let decoded = decode(url.as_str()).expect("UTF-8");
+        dbg!(&decoded);
+        get_content_async(decoded.to_string())
+    });
+
+    // proxy ts
+    let ts_proxy_router = warp::path!("ts" / String).and_then(move |url| {
+        get_content_async_ts(url)
+    });
+
+    let routers = m3u8_proxy_router.or(ts_proxy_router).with(cors);
+    warp::serve(routers).run(([127, 0, 0, 1], 25011)).await;
 }
 
-fn get_default_http_client() -> reqwest::Client {
-    let headers = HeaderMap::new();
-    return reqwest::Client::builder().default_headers(headers).connect_timeout(Duration::from_secs(8)).build().unwrap();
-}
-//
-// async fn get_content() -> String {
-//     let url = "https://live.v1.mk/api/bestv.php?id=cctv1hd8m/8000000";
-//     // let rt = tokio::runtime::Runtime::new().unwrap();
-//     // rt.block_on(async {
-//     let client = get_default_http_client();
-//     let result = client.get(url).send().await;
-//     let content = result.unwrap().text().await;
-//     content.unwrap()
-//     // })
-// }
-
-async fn get_content_async() -> Result<impl warp::Reply, Infallible> {
-    let url = "https://live.v1.mk/api/bestv.php?id=cctv1hd8m/8000000";
-    // let url = "http://39.135.138.58:18890/PLTV/88888888/224/3221225918/index.m3u8";
+async fn get_content_async(url: String) -> Result<impl warp::Reply, Infallible> {
+    // let url = "https://live.v1.mk/api/bestv.php?id=cctv1hd8m/8000000";
+    // // let url = "http://39.135.138.58:18890/PLTV/88888888/224/3221225918/index.m3u8";
+    // let url = "https://live.v1.mk/api/sxg.php?id=CCTV-6H265_4000";
     let client = get_default_http_client();
     let result = client.get(url).send().await;
     let content = result.unwrap().text().await;
@@ -46,5 +43,30 @@ async fn get_content_async() -> Result<impl warp::Reply, Infallible> {
         .body(m3u8)
         .unwrap();
     Ok(res)
+}
+
+async fn get_content_async_ts(ts: String) -> Result<impl warp::Reply, Infallible> {
+    // let url = "https://live.v1.mk/api/bestv.php?id=cctv1hd8m/8000000";
+    // let url = "http://39.135.138.58:18890/PLTV/88888888/224/3221225918/index.m3u8";
+    // let url = "http://test.8ne5i.10.vs.rxip.sc96655.com/live/8ne5i_sccn,CCTV-6H265_hls_pull_4000K/280/085/429.ts";
+    let client = get_default_http_client();
+    let result = client.get(ts).send().await;
+    let response = result.unwrap();
+    let content = response.text().await;
+    let ts = content.unwrap();
+    dbg!(&ts);
+    let res = Response::builder()
+        .status(200)
+        // Are you sure about this one? More like "text/plain"?
+        .header("Content-Type", "video/mp2t")
+        .body(ts)
+        .unwrap();
+    Ok(res)
+}
+
+
+fn get_default_http_client() -> reqwest::Client {
+    let headers = HeaderMap::new();
+    return reqwest::Client::builder().default_headers(headers).connect_timeout(Duration::from_secs(8)).build().unwrap();
 }
 
